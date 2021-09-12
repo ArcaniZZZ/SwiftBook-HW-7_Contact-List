974e3e1e4b98c06549c5e41fe73c186b093d61c7
Alex <arcani91@gmail.com>
2021-09-12 14:56:04 +0300

    Initial Commit

---
+++ SwiftBook HW 7_Contact List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftBook HW 7_Contact List
+//
+//  Created by Arcani on 12.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
